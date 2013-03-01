072a54e749724dad469342969cca768f97688b5c
Vital Belikov <vital@qwe.lv>
2013-03-01 18:48:59 +0200

    server cache

--- sitemenu/sitemenu_settings.py
+++ sitemenu/sitemenu_settings.py
@@ -8,5 +8,27 @@ PAGES = getattr(settings, 'SITEMENU_PAGES', (
 
 MENUCLASS = getattr(settings, 'SITEMENU_MENUCLASS', 'sitemenu.models.Menu')
 
+# SERVER CACHE
+
 SERVER_CACHE_DIR = getattr(settings, 'SITEMENU_SERVER_CACHE_DIR', None)
 SERVER_CACHE_ARGS_FUNC = getattr(settings, 'SITEMENU_SERVER_CACHE_ARGS_FUNC', None)
+
+# You need to invalidate cache!
+# basic method:
+
+# from django.db.models.signals import post_save
+# from django.dispatch import receiver
+# from sitemenu import sitemenu_settings
+
+# from django.contrib.sessions.models import Session
+# from qshop.cart.models import Cart, Item, Order
+# import shutil
+
+# if sitemenu_settings.SERVER_CACHE_DIR:
+#     skip_save_classes = (Session, Cart, Item, Order)
+
+#     @receiver(post_save)
+#     def clear_cache_after_save(sender, **kwargs):
+#         if not isinstance(kwargs['instance'], skip_save_classes):
+#             #print kwargs['instance']
+#             shutil.rmtree(sitemenu_settings.SERVER_CACHE_DIR, True)
