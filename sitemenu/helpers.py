83687e7fabd8f5fe4f1076e8d57b364ff6978757
Vital Belikov <vital@qwe.lv>
2015-01-13 17:48:24 +0200

    upload_to_slugify

--- sitemenu/helpers.py
+++ sitemenu/helpers.py
@@ -1,6 +1,15 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import Http404
 
+try:
+    from pytils.translit import translify
+except ImportError:
+   translify = lambda x: x
+import re
+slugify_replace_pattern = re.compile('[^\w\.]+')
+
+
+
 def get_paginated_list(objects, page, on_page):
     paginator = Paginator(objects, on_page)
 
@@ -19,3 +28,13 @@ def get_client_ip(request):
     else:
         ip = request.META.get('REMOTE_ADDR')
     return ip
+
+def slugify_and_transliterate(string):
+    return slugify_replace_pattern.sub('_', translify(string))
+
+
+def upload_to_slugify(basedir):
+    def make_upload_path(instance, filename):
+        filename_slug = slugify_and_transliterate(filename)
+        return u"%s/%s" % (basedir, filename_slug)
+    return make_upload_path
