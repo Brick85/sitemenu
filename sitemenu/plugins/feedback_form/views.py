f83c86dad324c48aa44121948f0f60c932f25fdd
Vitālijs Gaičuks <vitalijs.gaicuks@gmail.com>
2017-01-17 16:06:32 +0200

    Feedback form django 1.10 support

--- sitemenu/plugins/feedback_form/views.py
+++ sitemenu/plugins/feedback_form/views.py
@@ -1,11 +1,13 @@
-from django.http import HttpResponseRedirect
-from django.contrib import messages
-from django.shortcuts import render
-from django.utils.translation import ugettext_lazy as _
+#from forms import FeedbackFormForm
 from sitemenu.sitemenu_settings import PLUGIN_FEEDBACK_FORM
 from sitemenu import import_item
-
 FeedbackFormForm = import_item(PLUGIN_FEEDBACK_FORM)
+
+from django.http import HttpResponseRedirect
+from django.template import RequestContext
+from django.contrib import messages
+from django.shortcuts import render_to_response
+from django.utils.translation import ugettext_lazy as _
 
 
 def save_feedback_form(request):
@@ -14,16 +16,13 @@ def save_feedback_form(request):
         if form.is_valid():
             form.save()
             if request.is_ajax():
-                return render(request, 'sitemenu/plugins/feedback_form/feedback_form_sent.html')
+                return render_to_response('sitemenu/plugins/feedback_form/feedback_form_sent.html', context_instance=RequestContext(request))
             else:
                 messages.add_message(request, messages.INFO, _('Feedback form sent.'))
                 return HttpResponseRedirect('/')
     else:
         form = FeedbackFormForm(request=request)
 
-    return render(
-        request,
-        'sitemenu/plugins/feedback_form/feedback_form.html', {
+    return render_to_response('sitemenu/plugins/feedback_form/feedback_form.html', {
             'form': form
-        }
-    )
+        }, context_instance=RequestContext(request))
