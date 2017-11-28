f5ed59293d2e16da3ed6b7fca09fdaa37dfe5594
Vital Belikov <vital@qwe.lv>
2017-11-28 19:41:23 +0200

    python3 compatibility

--- sitemenu/__init__.py
+++ sitemenu/__init__.py
@@ -6,10 +6,10 @@ def import_item(path):
         render_import = path.split('.')
         render_module = importlib.import_module('.'.join(render_import[:-1]))
         render_function = render_import[-1]
-    except ImportError, e:
+    except ImportError as e:
         raise ImportError("Could not import function '%s': %s" % (path, e))
     try:
         func = getattr(render_module, render_function)
-    except AttributeError, e:
+    except AttributeError as e:
         raise AttributeError("Could not find function '%s' in %s" % (render_function, render_module))
     return func
