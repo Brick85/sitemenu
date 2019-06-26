da1610f388ec2762893bfca4b74deed2a2170e93
Vital Belikov <vital@qwe.lv>
2019-06-26 15:17:19 +0300

    django url -> path

--- sitemenu/plugins/feedback_form/urls.py
+++ sitemenu/plugins/feedback_form/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls import url
+from django.conf.urls import path
 from .views import save_feedback_form
 
 urlpatterns = [
-    url(r'^send/$', save_feedback_form, name='save_feedback_form'),
+    path('send/', save_feedback_form, name='save_feedback_form'),
 ]
